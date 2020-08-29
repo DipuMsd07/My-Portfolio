5ceccdf8575cb95dc4eac8b0d07c0a54f4625c4c
Deepak Sarode <dsarode013@gmail.com>
2020-08-29 11:52:07 +0530

    Add files via upload

---
+++ Js/hover.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector('.header .navbar .navmenu .hamburger');
+const mobile_menu = document.querySelector('.header .navbar .navmenu ul');
+const menu_item = document.querySelectorAll('.header .navbar .navmenu ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+
+});
+
+document.addEventListener('scroll', ()=>{
+
+    var scroll_position = window.scrollY;
+    if(scroll_position > 50){
+
+        header.style.backgroundColor = "#29323c";
+    }
+    else{
+
+        header.style.backgroundColor = "transparent";
+    }
+
+});
+
+menu_item.forEach(item=>{
+
+    item.addEventListener('click',()=>{
+
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+        
+    });
+
+
+});
