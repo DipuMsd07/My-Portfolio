03320e7fd6cbf608c731198ae56e1cd6a247bd8a
Deepak Sarode <dsarode013@gmail.com>
2021-02-16 14:01:51 +0530

    img

--- Js/hover.js
+++ Js/hover.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const hamburger = document.querySelector('.header .navbar .navmenu .hamburger');
 const mobile_menu = document.querySelector('.header .navbar .navmenu ul');
 const menu_item = document.querySelectorAll('.header .navbar .navmenu ul li a');
@@ -34,4 +35,42 @@ menu_item.forEach(item=>{
     });
 
 
+=======
+const hamburger = document.querySelector('.header .navbar .navmenu .hamburger');
+const mobile_menu = document.querySelector('.header .navbar .navmenu ul');
+const menu_item = document.querySelectorAll('.header .navbar .navmenu ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+
+});
+
+document.addEventListener('scroll', ()=>{
+
+    var scroll_position = window.scrollY;
+    if(scroll_position > 50){
+
+        header.style.backgroundColor = "#29323c";
+    }
+    else{
+
+        header.style.backgroundColor = "transparent";
+    }
+
+});
+
+menu_item.forEach(item=>{
+
+    item.addEventListener('click',()=>{
+
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+        
+    });
+
+
+>>>>>>> 346157d (First Commit)
 });
